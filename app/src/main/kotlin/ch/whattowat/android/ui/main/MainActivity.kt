ef59f4845800a46614924fc61e50b409b91d289d
Niall Scott <riverniall@gmail.com>
2017-06-05 22:41:54 +0100

    Added Kotlin language to the project. Added MainActivity.

---
+++ app/src/main/kotlin/ch/whattowat/android/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package ch.whattowat.android.ui.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import ch.whattowat.android.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
